b3a38612df1fe06dccfdc9a6315d3ca7471ad41a
Kiefer Luttrell <kieferluttrell@gmail.com>
2022-07-05 09:17:38 -0500

    navBar changes

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -4,34 +4,41 @@ import "./NavBar.css"
 export const NavBar = () => {
     const navigate = useNavigate()
     return <>
-        <div className="logo__navbar">
-        <img  src="https://ncwc.edu/wp-content/uploads/2016/06/library-icon.png" height={"50px"} width={"50px"}/>
-        <div className="logo__navbar__title">Digital Library</div>
+        <div className="navBarContainer">
+            
+            <a className="navbar">
+                <div className="logoImg">
+                <img src="https://ncwc.edu/wp-content/uploads/2016/06/library-icon.png" height={"35px"} width={"35px"} />
+                </div>
+                <div className="logo__navbar__title">Digital Library</div>
+                <div className="navBar_item_container">
+                <a className="navbar__item active">
+                    <Link className="navbar__link" to="/home">Home</Link>
+                </a>
+                <a className="navbar__item active">
+                    <Link className="navbar__link" to="/library">Library</Link>
+                </a>
+                <a className="navbar__item active">
+                    <Link className="navbar__link" to="/profile">Profile</Link>
+                </a>
+                <a className="navbar__item active">
+                    <Link className="navbar__link" to="library/myList"> My List</Link>
+                </a>
+                </div>
+                <div className="logout__">
+                {
+                    localStorage.getItem("library_user")
+                        ? <a className="navbar__item navbar__logout">
+                            <Link className="navbar__link" to="" onClick={() => {
+                                localStorage.removeItem("library_user")
+                                navigate("/", { replace: true })
+                            }}>Logout</Link>
+                        </a>
+                        : ""
+                }
+                </div>
+            </a>
         </div>
-        <ul className="navbar">
-             <li className="navbar__item active">
-                <Link className="navbar__link" to="/home">Home</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/library">Library</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/profile">Profile</Link>
-            </li>
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="library/myList"> My List</Link>
-            </li>
-            {
-                localStorage.getItem("library_user")
-                    ? <li className="navbar__item navbar__logout">
-                        <Link className="navbar__link" to="" onClick={() => {
-                            localStorage.removeItem("library_user")
-                            navigate("/", {replace: true})
-                        }}>Logout</Link>
-                    </li>
-                    : ""
-            }
-        </ul>
-    
+
     </>
 }
